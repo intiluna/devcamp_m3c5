f0e39bb11fea66510109dea5bc017dcde06df97e
Inti Luna <inlua@alumni.uv.es>
2025-03-24 13:42:25 +0100

    codigo p1

---
+++ python_assignment.py
@@ -0,0 +1,25 @@
+#1. Cree un bucle For de Python.
+
+# 1.1 Ejemplo de bucle for
+print("Ejemplo 1 de bucle for")
+for num in range(11):
+    print(num)
+
+print("Ejemplo 2 de bucle for")
+
+mi_lista = ["arroz", "pescado","aceite","sal","camarones"]
+
+for ingrediente in mi_lista:
+    print(f"Para cocinar el siguiente plato se necesita: {ingrediente}")
+
+print("-" * 50)
+#Cree una función de Python llamada suma que tome 3 argumentos y devuelva la suma de los 3.
+
+#Cree una función lambda con la misma funcionalidad que la función de suma que acaba de crear.
+
+#Utilizando la siguiente lista y variable, determine si el valor de la variable coincide o no con un valor de la lista.
+# *Sugerencia, si es necesario, utilice un bucle for in y el operador in.
+
+nombre = 'Enrique'
+
+lista_nombre = 'Jessica', 'Paul', 'George', 'Henry', 'Adán'
